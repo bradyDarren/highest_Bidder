eba3b25c3c86fb4cc1332152d3fced525d2452a0
bradyDarren <dcgbfinancial@gmail.com>
2024-10-11 14:16:48 -0500

    trying to find a solution to pull the max value from the dictionary

--- highestBidder.py
+++ highestBidder.py
@@ -1,19 +1,23 @@
 
+# dictonary holds key value pairs input by the user
+bidders = {"Tommy":101, "Carl": 102, "Darren": 120}
 
-bidders = {}
-
+# accepts input from user and populates the bidder dictionary 
 def bid():
-    more_bids = "yes"
-    while more_bids != "no":
-        user_name = input("Enter your name: ")
-        user_bid = int(input("Enter your desired bid amount: "))
-        bidders[user_name] = user_bid
-        print(bidders)
-        more_bids = input("Are there any more bidders? Type 'yes or no'. ").lower()
+    # more_bids = "yes"
+    # while more_bids != "no":
+    #     user_name = input("Enter your name: ")
+    #     user_bid = input("Enter your desired bid amount: ")
+    #     bidders[user_name] = user_bid
+    #     print(bidders)
+    #     more_bids = input("Are there any more bidders? Type 'yes or no'. ").lower()
     
     for key in bidders:
-        print(bidders[key])
-                
+        max_bid = max(bidders)
+        a = bidders.get("Tommy")
+    print(max_bid)
+    print(a)
+          
 
 
 bid()
