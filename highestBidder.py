eaebadc5aaa722fbd2d5f7d3456828400498bce5
bradyDarren <dcgbfinancial@gmail.com>
2024-10-11 16:38:05 -0500

    seperation into two functions, and restructuring.

--- highestBidder.py
+++ highestBidder.py
@@ -1,27 +1,28 @@
 
 # dictonary holds key value pairs input by the user
-bidders = {"Tommy":101, "Carl": 102, "Darren": 120}
+bidders = {}
+
 
 # accepts input from user and populates the bidder dictionary 
 def bid():
-    # more_bids = "yes"
-    # while more_bids != "no":
-    #     user_name = input("Enter your name: ")
-    #     user_bid = input("Enter your desired bid amount: ")
-    #     bidders[user_name] = user_bid
-    #     print(bidders)
-    #     more_bids = input("Are there any more bidders? Type 'yes or no'. ").lower()
-    
-    for key in bidders:
-        max_bid = max(bidders)
-        a = bidders.get("Tommy")
-    print(max_bid)
-    print(a)
-          
+    more_bids = "yes"
+    while more_bids != "no":
+        user_name = input("Enter your name: ")
+        user_bid = input("Enter your desired bid amount: ")
+        bidders[user_name.strip()] = user_bid.strip()
+        print(bidders)
+        more_bids = input("Are there any more bidders? Type 'yes or no'. ").lower()
+    return bidders 
+
+final_list = bid()
+
+# find the max value within the created dictionary.
+def results(bidder_list):
+    for key in bidder_list:
+        max_bidder = max(bidder_list, key = bidder_list.get)
+        max_bid = bidder_list[max_bidder]
+    return f"Sold to {max_bidder} for the price of ${max_bid}."
 
 
-bid()
-
-
-
+print(results(final_list))
 
